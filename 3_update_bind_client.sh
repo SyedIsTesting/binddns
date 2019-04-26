c3f65d229cab1b9a1ebbe8760975d0f0207c4080
SyedIsTesting <36777770+SyedIsTesting@users.noreply.github.com>
2019-04-26 12:29:48 +0100

    Update 3_update_bind_client.sh

--- 3_update_bind_client.sh
+++ 3_update_bind_client.sh
@@ -2,8 +2,8 @@
 zone="hassnat.co.uk"
 keyvalue="$(az keyvault secret show --name "rndc-key" --vault-name "CustomerKV1" --query value)"
 dnsserver="10.22.113.137"
-wget https://github.com/SyedIsTesting/binddns/blob/master/rndc.key -P /etc
-sed -i "s|secret.*|${myString}secret $keyvalue;|g" /etc/rndc.key
+wget https://github.com/SyedIsTesting/binddns/blob/master/rndc-dns.key -P /etc
+sed -i "s|secret.*|${myString}secret $keyvalue;|g" /etc/rndc-dns.key
 serverip="$(hostname -I | cut -d" " -f 1)"
 hostname="$(cat /proc/sys/kernel/hostname)"
 sed -i 's/nameserver.*/nameserver 10.22.113.137/g' /etc/resolv.conf
@@ -13,4 +13,4 @@ zone $zone
 update add $hostname.$zone 86400 A $serverip
 send
 EOF
-nsupdate -v -k /etc/rndc.key updatedns
+nsupdate -v -k /etc/rndc-dns.key updatedns
